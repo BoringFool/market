49c7efaab1cc6c8ecdfa4a5c12e117c1e326c013
BoringFool <1318593330@qq.com>
2018-03-01 07:44:15 +0800

    .
    表的设计有点烦

---
+++ src/com/zm/model/Attribute.java
@@ -0,0 +1,17 @@
+package com.zm.model;
+
+import java.util.List;
+
+public class Attribute {
+	private Long id;
+	private List<String> attrValue;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+}
--- src/com/zm/model/Goods.java
+++ src/com/zm/model/Goods.java
@@ -1,5 +1,7 @@
 package com.zm.model;
 
+import java.util.List;
+
 public class Goods {
 	private Long id;
 	private String name;
@@ -7,4 +9,61 @@ public class Goods {
 	private String imgeurl;
 	private Double price;
 	private String store;
+	private List<Attribute> attributes;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	public String getImgeurl() {
+		return imgeurl;
+	}
+
+	public void setImgeurl(String imgeurl) {
+		this.imgeurl = imgeurl;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public String getStore() {
+		return store;
+	}
+
+	public void setStore(String store) {
+		this.store = store;
+	}
+
+	public List<Attribute> getAttributes() {
+		return attributes;
+	}
+
+	public void setAttributes(List<Attribute> attributes) {
+		this.attributes = attributes;
+	}
 }
