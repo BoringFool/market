d6e7167bc5087b0f4403b71e69fe04dafa9c0e99
BoringFool <1318593330@qq.com>
2018-05-29 07:34:44 +0800

    .
    静态资源被拦截了，需要配置mvc

---
+++ src/com/zm/interceptor/TestInterceptor.java
@@ -0,0 +1,58 @@
+package com.zm.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class TestInterceptor extends HandlerInterceptorAdapter {
+
+	@Override
+	public boolean preHandle(HttpServletRequest request,
+			HttpServletResponse response, Object handler) throws Exception {
+		if ("GET".equalsIgnoreCase(request.getMethod())) {
+			System.out.println("get方式");
+		}
+
+		System.out.println("==============执行顺序: 1、preHandle================");
+		String requestUri = request.getRequestURI();
+		String contextPath = request.getContextPath();
+		String url = requestUri.substring(contextPath.length());
+
+		if ("/userController/login".equals(url)) {
+			return true;
+		} else {
+			String username = (String) request.getSession()
+					.getAttribute("user");
+			if (username == null) {
+				System.out.println("Interceptor：跳转到login页面！");
+				request.getRequestDispatcher("/page/index.jsp").forward(
+						request, response);
+				return false;
+			} else {
+				return true;
+			}
+		}
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request,
+			HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		System.out.println("==============执行顺序: 2、postHandle================");
+		if (modelAndView != null) { // 加入当前时间
+			modelAndView.addObject("haha", "测试postHandle");
+		}
+		super.postHandle(request, response, handler, modelAndView);
+	}
+
+	@Override
+	public void afterCompletion(HttpServletRequest request,
+			HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+		System.out
+				.println("==============执行顺序: 3、afterCompletion================");
+		super.afterCompletion(request, response, handler, ex);
+	}
+}
